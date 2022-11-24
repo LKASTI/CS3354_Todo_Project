36fa07c668d3e7014508c68c67f0bdb6cfc86d21
Landin Kasti <lkastiman@gmail.com>
2022-11-24 09:49:03 -0600

    ..

--- src/pages/CreateTask.js
+++ src/pages/CreateTask.js
@@ -1,11 +1,13 @@
 import { useContext } from 'react'
 import {AppContext} from '../App'
+import react from 'react'
 import * as yup from 'yup'
 import {useForm} from 'react-hook-form'
 import {yupResolver} from '@hookform/resolvers/yup'
 import {addDoc, getDocs, doc, setDoc, collection} from 'firebase/firestore'
 import {db} from '../config/firebase'
-import "../TaskCreation.css"
+import Box from '@mui/material/Box'
+// import "../TaskCreation.css"
 import { useNavigate } from 'react-router-dom'
 
 function CreateTask(){
@@ -42,19 +44,25 @@ function CreateTask(){
     
     return(
         <div className='task-creation'>
-            {console.log(taskList)}
+            {/* {console.log(taskList)} */}
             <form onSubmit={handleSubmit(onSubmit)}>
-                <h1>Create a Task</h1>
-                <label>Task Name</label>
-                <input type="text" placeholder='ex) Take Out Trash' autoComplete='off' {...register("taskname")}/>
-                <p style={{color:"red"}}>{errors.taskname?.message}</p>
-                <label>Days to Complete</label>
-                <input type="number" placeholder='ex) 5' autoComplete='off' {...register("timeleft")}/>
-                <p style={{color:"red"}}>{errors.timeleft?.message}</p>
-                <label>Task Details</label>
-                <textarea placeholder='ex) Take out the trash in the living room and the kitchen' autoComplete='off' {...register("description")}/>
-                <p style={{color:"red"}}>{errors.description?.message}</p>
-                <input type="submit"/> 
+                <Box>
+                    Create a Task
+                </Box>
+                <Box>
+                    <label>Task Name</label>
+                    <input type="text" placeholder='ex) Take Out Trash' autoComplete='off' {...register("taskname")}/>
+                    <p style={{color:"red"}}>{errors.taskname?.message}</p>
+                    <label>Days to Complete</label>
+                    <input type="number" placeholder='ex) 5' autoComplete='off' {...register("timeleft")}/>
+                    <p style={{color:"red"}}>{errors.timeleft?.message}</p>
+                    <label>Task Details</label>
+                    <textarea placeholder='ex) Take out the trash in the living room and the kitchen' autoComplete='off' {...register("description")}/>
+                    <p style={{color:"red"}}>{errors.description?.message}</p>
+                </Box>
+                <Box>
+                    <input type="submit"/> 
+                </Box>
             </form>
         </div>
     )
